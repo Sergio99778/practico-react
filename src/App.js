ae0a415a6ce50fa3ad7f7cb03a76c287693e70b4
Sergio99778 <sergio4brandon@gmail.com>
2022-02-01 23:31:53 -0600

    Addins user state and route for Create component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Components
 import Login from "./pages/Login";
 import Password from "./pages/Password";
+import Create from "./pages/Create";
 
 function App() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route
+          exact
+          path="/"
+          element={<Login user={user} setUser={setUser} />}
+        />
         <Route exact path="/password" element={<Password />} />
+        <Route exact path="/create" element={<Create />} />
       </Routes>
     </Router>
   );
